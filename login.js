9541f0381f73ac8d6f89a252ffc96274d215e69c
Thales Spanhol <thales_spanhol@hotmail.com>
2024-10-05 21:55:54 -0300

    format

--- login.js
+++ login.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const loginModal = document.getElementById('loginModal');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const loginModal = document.getElementById("loginModal");
 
-    const storedUsername = 'admin';
-    const storedPassword = '12345';
+  const storedUsername = "admin";
+  const storedPassword = "12345";
 
-    // Mostrar o modal quando a página carregar
-    loginModal.style.display = 'flex';
+  // Mostrar o modal quando a página carregar
+  loginModal.style.display = "flex";
 
-    // Verificar as credenciais ao enviar o formulário
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+  // Verificar as credenciais ao enviar o formulário
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
 
-        if (username === storedUsername && password === storedPassword) {
-            loginModal.style.display = 'none';
-        } else {
-            alert('Usuário ou senha incorretos!');
-        }
+      if (username === storedUsername && password === storedPassword) {
+        loginModal.style.display = "none";
+      } else {
+        alert("Usuário ou senha incorretos!");
+      }
     });
 });
